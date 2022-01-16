3fcc3ee7fff5c18d1443478f24f953057d431cb7
Rudnitski <byshlata@yandex.ru>
2022-01-16 16:06:00 +0300

    first commit

---
+++ src/Components/Games/CatalogGame/Duble/YouLoser/YouLuser.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import s from "./YouWin.module.sass"
+import {SmileCat} from "./SmileCat";
+
+
+
+export const YouWin = () => {
+    return(
+        <SmileCat/>
+    )
+}
